72e6f2ea11fae3316ccadd1c717f49bb668b2057
Lay András <andras@lay.hu>
2025-06-18 21:48:34 +0200

    Remove temporary files

--- install.sh
+++ install.sh
@@ -16,4 +16,6 @@ mkdir web/pma
 wget -P /tmp https://files.phpmyadmin.net/phpMyAdmin/5.2.2/phpMyAdmin-5.2.2-english.tar.gz
 tar -xzf /tmp/phpMyAdmin-5.2.2-english.tar.gz -C /tmp
 cp -R /tmp/phpMyAdmin-5.2.2-english/* web/pma
+rm -R /tmp/phpMyAdmin-5.2.2-english /tmp/phpMyAdmin-5.2.2-english.tar.gz
+
 cp docker/php/config.inc.php web/pma
