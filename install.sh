b05f82c0e7eb31177ce80031f32b75f8c517b0b6
Lay András <andras@lay.hu>
2025-06-18 23:21:01 +0200

    Change to more secure EC algo.

--- install.sh
+++ install.sh
@@ -1,16 +1,13 @@
 rm -Rf ssl
 mkdir ssl
-#openssl genrsa -out ssl/ca.key 2048
-openssl ecparam -name secp384r1 -genkey -noout -out ssl/ca.key
+openssl ecparam -name secp521r1 -genkey -noout -out ssl/ca.key
 openssl req -x509 -new -nodes -key ssl/ca.key -subj "/CN=MyCA" -days 365 -out ssl/ca.crt
 
-#openssl genrsa -out ssl/server.key 2048
-openssl ecparam -name secp384r1 -genkey -noout -out ssl/server.key
+openssl ecparam -name secp521r1 -genkey -noout -out ssl/server.key
 openssl req -new -key ssl/server.key -subj "/CN=mariadb" -out ssl/server.csr
 openssl x509 -req -in ssl/server.csr -CA ssl/ca.crt -CAkey ssl/ca.key -set_serial 100 -out ssl/server.crt -days 365
 
-#openssl genrsa -out ssl/client.key 2048
-openssl ecparam -name secp384r1 -genkey -noout -out ssl/client.key
+openssl ecparam -name secp521r1 -genkey -noout -out ssl/client.key
 openssl req -new -key ssl/client.key -subj "/CN=php-app" -out ssl/client.csr
 openssl x509 -req -in ssl/client.csr -CA ssl/ca.crt -CAkey ssl/ca.key -set_serial 101 -out ssl/client.crt -days 365
 
